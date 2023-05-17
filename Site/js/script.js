78e7768532b127ba926229c2828588ce5bdfdf4c
Ahmet Yasin Pekdemir <109309350+Lotsiron@users.noreply.github.com>
2023-05-17 12:53:11 +0300

    Şehrim içerik doldurma.
    Tıklanabilir resimler kendi sayfalarına yönlendiriyor.

--- Site/js/script.js
+++ Site/js/script.js
@@ -14,9 +14,44 @@
     if (document.documentElement.scrollTop > 120) 
     {
       document.getElementById("header").style.height = "50px";
+      document.getElementById("Logo").style.height = "50px";
     } 
     else if (document.documentElement.scrollTop < 60) 
     {
       document.getElementById("header").style.height = "120px";
+      document.getElementById("Logo").style.height = "100px";
     }
   } 
+
+  //Şehir resimleri
+  let jSlideSayi = 1;
+  slideGoster(jSlideSayi);
+  
+  // sonraki/önceki kontrolleri
+  function slideArttir(n) {
+    slideGoster(jSlideSayi += n);
+  }
+  
+  // Küçük resim kontrolleri
+  function SlideSabit(n) {
+    slideGoster(jSlideSayi = n);
+  }
+
+
+  function slideGoster(n) {
+    let i;
+    let jslides = document.getElementsByClassName("slide");
+    let jkucukresim = document.getElementsByClassName("kucukresim");
+    if (n > jslides.length) {jSlideSayi = 1}
+    if (n < 1) {jSlideSayi = jslides.length}
+    for (i = 0; i < jslides.length; i++) {
+      jslides[i].style.display = "none";
+    }
+    for (i = 0; i < jkucukresim.length; i++) {
+      jkucukresim[i].className = jkucukresim[i].className.replace(" active", "");
+    }
+    jslides[jSlideSayi-1].style.display = "block";
+    jkucukresim[jSlideSayi-1].className += " active";
+  }
+
+  
